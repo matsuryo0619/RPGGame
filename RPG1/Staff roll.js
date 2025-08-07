127cada4cf138ecf5b3b7cce5d19346f4bdecf79
MatsuR <ryo.matsu0619@gmail.com>
2025-08-07 22:13:59 +0900

    Update Staff roll.js

--- RPG1/Staff roll.js
+++ RPG1/Staff roll.js
@@ -1,1 +1,25 @@
+const Text = `
+スタッフロール
 
+制作スタッフ
+
+ドット絵: MatsuR
+
+ストーリー制作: MatsuR, ScenarioSmith(ChatGPT)
+
+プログラム: MatsuR, ScenarioSmith(ChatGPT), Claude
+
+BGM / SE: 魔王魂 さま
+
+特別協力:
+
+ChatGPT
+
+Claude
+
+使用ツール
+
+GitHub
+
+GitHub Pages
+`;
