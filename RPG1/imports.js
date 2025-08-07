82ec0d96e0fa6975bd8cc259f213c92a2f4f67fa
MatsuR <ryo.matsu0619@gmail.com>
2025-08-07 22:56:00 +0900

    Update imports.js

--- RPG1/imports.js
+++ RPG1/imports.js
@@ -1,2 +1,8 @@
+CanvasRenderingContext2D.prototype.fillRectColor = function(x, y, w, h, c) {
+  const ctx = this;
+  ctx.fillStyle = c;
+  ctx.fillRect(x, y, w, h);
+};
+
 const canvas = document.getElementById('gameWindow');
 export const ctx = canvas.getContext('2d');
