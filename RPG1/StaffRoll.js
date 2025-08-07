09235b424385eb8cabdfd4af3c89b02d1189bf4a
MatsuR <ryo.matsu0619@gmail.com>
2025-08-08 00:15:59 +0900

    Update StaffRoll.js

--- RPG1/StaffRoll.js
+++ RPG1/StaffRoll.js
@@ -30,6 +30,11 @@ GitHub Pages
 `;
 
 export function Staffroll() {
-  // 中央揃えならxを canvas.width / 2 にするべし！
-  ctx.fillTextOptions(Text, canvas.width / 2, 40, 'black', '25px', 'sans-serif', 'center', 'top');
+  if (typeof Staffroll.scrollY === 'undefined') {
+    Staffroll.scrollY = canvas.height;
+  }
+
+  ctx.fillTextOptions(Text, canvas.width / 2, Staffroll.scrollY, 'black', '25px', 'sans-serif', 'center', 'top');
+
+  Staffroll.scrollY -= 1;
 }
