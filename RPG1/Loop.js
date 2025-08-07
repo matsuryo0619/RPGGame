40246489dc9e63bb65130548541ba728c7e3c9f4
MatsuR <ryo.matsu0619@gmail.com>
2025-08-07 23:58:00 +0900

    Update Loop.js

--- RPG1/Loop.js
+++ RPG1/Loop.js
@@ -1,9 +1,13 @@
+import { ctx, canvas } from 'imports.js';
 import { Staffroll } from 'Staff roll.js';
 
 let currentScreen;
 
 function loop() {
   requestAnimationFrame(loop);
+
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.fillRectColor(0, 0, canvas.width, canvas.height, 'white');
 
   if (currentScreen === 'Staffroll') {
     Staffroll();
