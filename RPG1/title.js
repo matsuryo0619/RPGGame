5ad39249e4bdc082fc0b611b1fd146e8a7b4b87c
MatsuR <ryo.matsu0619@gmail.com>
2025-08-08 16:05:21 +0900

    Update title.js

--- RPG1/title.js
+++ RPG1/title.js
@@ -4,7 +4,8 @@ export function title() {
   if (typeof title.BackHeight !== 'number') {
     title.BackHeight = 0;
   }
-  
+
+  ctx.globalAlpha = 0.5;
   ctx.fillRoundedRect(Width / 2, Height / 2 + 100, 200, Math.min(title.BackHeight, 175), 20, 'black');
 
   title.BackHeight += 5;
