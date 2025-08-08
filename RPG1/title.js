0ef19ee14960091d1f1b224c304d2877f0864c8b
MatsuR <ryo.matsu0619@gmail.com>
2025-08-08 18:12:48 +0900

    Update title.js

--- RPG1/title.js
+++ RPG1/title.js
@@ -1,6 +1,11 @@
-import { ctx, canvas, Width, Height, State, keys } from './imports.js';
+const titleImage = new Image();
+titleImage.src = 'https://i.imgur.com/e0qKLwV.png';
 
 export function title() {
+  if (!titleImage.complete) return;
+
+  ctx.drawImage(titleImage, Width / 2 - titleImage.width / 2, 50);
+
   if (typeof title.BackHeight !== 'number') {
     title.BackHeight = 0;
   }
@@ -8,7 +13,7 @@ export function title() {
   ctx.globalAlpha = 0.5;
   ctx.fillRoundedRect(Width / 2, Height / 2 + 100, 200, title.BackHeight, 20, 'black');
   ctx.globalAlpha = 1;
-  
+
   title.BackHeight += (175 - title.BackHeight) / 15;
 
   if (175 <= title.BackHeight) {
