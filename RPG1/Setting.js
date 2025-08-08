a1d5239bc785607a66d13cea053042d6aa24efc2
MatsuR <ryo.matsu0619@gmail.com>
2025-08-08 20:24:05 +0900

    Update Setting.js

--- RPG1/Setting.js
+++ RPG1/Setting.js
@@ -1,5 +1,11 @@
-import { canvas, ctx, State, keys } from './imports.js';
+import { canvas, ctx, Width, Height, State, keys } from './imports.js';
 
 export function setting() {
-  
+  if (setting.Screen === 'undefined') {
+    setting.Screen = 'Name';
+  }
+
+  if (setting.Screen = 'Name') {
+    ctx.fillRoundedRect(Width / 2, Height / 2, 500, 500, 15, '#444');
+  }
 };
