1027eb615497018f3328aef3b1974cb9b1e80f3b
MatsuR <ryo.matsu0619@gmail.com>
2025-08-08 10:17:47 +0900

    Create transition.js

---
+++ RPG1/transition.js
@@ -0,0 +1,15 @@
+import { ctx, canvas, State } from './imports.js'
+
+function transition() {
+  if (typeof transition.alpha !== 'number') {
+    transition.alpha = 0;
+  }
+  transition.alpha += 0.02;
+  ctx.globalAlpha = transition.alpha;
+  ctx.fillRectColor(0, 0, canvas.width, canvas.height, 'black');
+  if (1 < transition.alpha) {
+    transition.alpha = 0;
+    State.currentScreen = State.nextScreen;
+    State.nextScreen = null;
+  };
+}
